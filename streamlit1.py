d35a64969a2cc9c560dad04c379e4aaacbcb1de9
naylorb <143852819+naylorb@users.noreply.github.com>
2023-12-24 11:37:40 -0500

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -88,7 +88,7 @@ with st.container():
 
     # Documention: https://formsubmit.co/ !!! CHANGE EMAIL ADDRESS !!!
     contact_form = """
-    <form action="formsubmit.co/naylorbmn@gmail.com" method="POST">
+    <form action="formsubmit.io/naylorbmn@gmail.com" method="POST">
         <input type="hidden" name="_captcha" value="false">
         <input type="text" name="name" placeholder="Your name" required>
         <input type="email" name="email" placeholder="Your email" required>
